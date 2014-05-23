09946cc02110428c83346ae2daaaaf5ed234bdaa
Ronaldo Ferreira de Lima <jimmy@cpan.org>
2014-05-23 17:51:19 +0000

    Import of JIMMY/App-Table2YAML-0.001 from CPAN.
    gitpan-cpan-distribution: App-Table2YAML
    gitpan-cpan-version:      0.001
    gitpan-cpan-path:         JIMMY/App-Table2YAML-0.001.tar.gz
    gitpan-cpan-author:       JIMMY
    gitpan-cpan-maturity:     released

---
+++ lib/App/Table2YAML/Loader/FixedWidth.pm
@@ -0,0 +1,39 @@
+package App::Table2YAML::Loader::FixedWidth;
+
+use common::sense;
+use charnames q(:full);
+use English qw[-no_match_vars];
+use Moo::Role;
+
+our $VERSION = '0.001'; # VERSION
+
+sub load_fixedwidth {...}
+
+no Moo;
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__END__
+
+=pod
+
+=encoding utf8
+
+=head1 NAME
+
+App::Table2YAML::Loader::FixedWidth - Plugin for I<Fixed-Width> files.
+
+=head1 VERSION
+
+version 0.001
+
+=head1 METHODS
+
+=head2 load_fixedwidth
+
+=head1 AUTHOR
+
+Ronaldo Ferreira de Lima aka jimmy <jimmy at gmail>.
+
+=cut
